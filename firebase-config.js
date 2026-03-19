1b56e5e4952ae8685ea6bdf9904e56967d9b5b2c
Aygerym <aygerymbatykova@gmail.com>
2026-03-19 05:53:19 +0500

    Обновление файлов для GitHub Paes

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+window.__firebase_config = {
+  apiKey: "AIzaSyAiTI_5BSkx5RLXzOJNd-Hz8YKeKSbK-sA",
+  authDomain: "student-d57b5.firebaseapp.com",
+  projectId: "student-d57b5",
+  storageBucket: "student-d57b5.firebasestorage.app",
+  messagingSenderId: "95443799229",
+  appId: "1:95443799229:web:edbb3a64cd7a09d5c03bd4",
+  measurementId: "G-RWK9B42594",
+};
